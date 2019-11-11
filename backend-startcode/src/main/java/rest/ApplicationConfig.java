6758bd4e1bdfd2417faf18df23d2e14536fb62a7
Sinan <sinanjasar@live.dk>
2019-11-11 18:25:24 +0100

    added endpoint

--- backend-startcode/src/main/java/rest/ApplicationConfig.java
+++ backend-startcode/src/main/java/rest/ApplicationConfig.java
@@ -22,10 +22,10 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.RenameMeResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
-        resources.add(rest.RenameMeResource.class);
     }
     
 }

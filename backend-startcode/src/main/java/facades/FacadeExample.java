1d6cdecb1937d73dfd680c5a0066bf43c6f82130
Sinan <sinanjasar@live.dk>
2019-11-11 17:43:13 +0100

    added facade for server fetch

--- backend-startcode/src/main/java/facades/FacadeExample.java
+++ backend-startcode/src/main/java/facades/FacadeExample.java
@@ -1,7 +1,22 @@
 package facades;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 import entities.RenameMe;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,10 +29,10 @@ public class FacadeExample {
 
     private static FacadeExample instance;
     private static EntityManagerFactory emf;
-    
+    final static String[] URLS = {"https://jsonplaceholder.typicode.com/comments/","https://jsonplaceholder.typicode.com/posts/",
+        "https://jsonplaceholder.typicode.com/albums/","https://jsonplaceholder.typicode.com/photos/","https://jsonplaceholder.typicode.com/todos/"};
     //Private Constructor to ensure Singleton
     private FacadeExample() {}
-    
     
     /**
      * 
@@ -45,7 +60,38 @@ public class FacadeExample {
         }finally{  
             em.close();
         }
-        
     }
-
+    
+    private static String fetchFromServers(String URL, String arg) throws MalformedURLException, ProtocolException, IOException {
+        URL url = new URL(URL + arg);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+        String jsonStr = "";
+        try (Scanner scan = new Scanner(con.getInputStream())) {
+            while (scan.hasNext()) {
+                jsonStr+=scan.nextLine();
+            }
+        }
+        return jsonStr;
+    }
+    public void getDataFromServers() throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        Queue<Future> futureList = new LinkedList();
+        List<String> allData = new ArrayList();
+        for(String URL : URLS) {
+            Future<String> future = executor.submit(()-> {
+                return fetchFromServers(URL,"1");
+            });
+            futureList.add(future);
+        }
+        while(!futureList.isEmpty()) {
+            Future<String> f = futureList.poll();
+            if(f.isDone()) {
+                allData.add(f.get());
+            } else {
+                futureList.add(f);
+            }
+        }
+    }
 }
